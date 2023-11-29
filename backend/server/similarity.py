5816dae9a81c4f64145d9313274edadb720f3c63
dianin5 <xyg1940@cu.ac.kr>
2023-11-29 23:16:17 +0900

    미디어 파일 : 이미지픽셀->이미지 해시값 비교로 변경

--- backend/server/similarity.py
+++ backend/server/similarity.py
@@ -4,14 +4,11 @@ import os
 from io import BytesIO
 from zipfile import ZipFile
 import xml.etree.ElementTree as ET
-import imagehash
-from PIL import Image
-import io
 
-app = Flask(__name__)
+
 similarity_bp = Blueprint('similarity', __name__)
 
-@app.route('/similarity', methods=['POST'])
+@similarity_bp.route('/similarity', methods=['POST'])   
 def similarity_analysis():
     data = request.json
     db_path = data['parsingDBpath']
@@ -20,20 +17,19 @@ def similarity_analysis():
     with sqlite3.connect(db_path) as conn:
         # 메타데이터 처리 및 해시 계산
         process_files(conn)
-        hashes, key_hashes = calculate_hashes_from_db(conn, key_document_id)
         # 유사한 미디어 파일이 포함된 문서 찾기
-        similar_media = find_similar_media(hashes, key_hashes, key_document_id)
+        similar_media = find_similar_media(conn, key_document_id)
         
         # 완전히 동일한 문서 찾기
         identical_documents = find_identical_documents(conn, key_document_id)
 
         # 메타데이터가 유사한 문서 찾기 (구현 필요)
-        similar_metadata_docs = find_similar_metadata_documents(conn, key_document_id)
+        #similar_metadata_docs = find_similar_metadata_documents(conn, key_document_id)
 
     result = {
         "identical_documents": identical_documents,
         "similar_media": similar_media,
-        "similar_metadata_documents": similar_metadata_docs
+        #"similar_metadata_documents": similar_metadata_docs
     }
 
     return jsonify(result)
@@ -56,7 +52,6 @@ def parse_metadata(blob_data, xml_file):
 # 데이터베이스에 메타데이터를 저장하는 함수
 def save_metadata(conn, filename, metadata):
     cursor = conn.cursor()
-
     # 이미 생성된 documentmetadata 테이블의 현재 컬럼을 확인
     cursor.execute("PRAGMA table_info(documentmetadata);")
     existing_columns = {info[1] for info in cursor.fetchall()}
@@ -89,47 +84,58 @@ def save_metadata(conn, filename, metadata):
 
 # OOXML 파일의 메타데이터를 처리하는 함수
 def process_files(conn):
+    conn.row_factory = sqlite3.Row  
     cursor = conn.cursor()
     cursor.execute("SELECT file_path, blob_data FROM files")
     rows = cursor.fetchall()
     for row in rows:
-        file_path, blob_data = row
+        file_path, blob_data = row['file_path'], row['blob_data']
         if is_ooxml(file_path):
             for xml_file in ['docProps/app.xml', 'docProps/core.xml']:
                 metadata = parse_metadata(blob_data, xml_file)
                 save_metadata(conn, file_path, metadata)
 
-# 이미지 파일들의 해시값을 계산하는 함수
-def calculate_hashes_from_db(conn, key_document_id):
-    hashes = {}
-    key_hashes = []
+def find_similar_media(conn, key_document_id):
+    similar_documents = {}
     cursor = conn.cursor()
-    cursor.execute("SELECT ID, DocumentID, FileData FROM MediaFiles")
-    media_files = cursor.fetchall()
 
-    for file_id, document_id, file_data in media_files:
-        try:
-            image = Image.open(io.BytesIO(file_data))
-            image_hash = imagehash.average_hash(image)
-            hashes[file_id] = (document_id, image_hash)
-            if str(document_id) == key_document_id:
-                key_hashes.append(image_hash)
-        except IOError:
-            print(f"File ID {file_id} is not an image or cannot be opened.")
-    
-    return hashes, key_hashes
+    # key_document_id에 해당하는 미디어 파일의 해시값과 총 개수를 가져옵니다.
+    cursor.execute("SELECT SHA256Hash FROM MediaFiles WHERE DocumentID = ?", (key_document_id,))
+    key_files_hashes = {row['SHA256Hash'] for row in cursor.fetchall()}
+    cursor.execute("SELECT COUNT(*) FROM MediaFiles WHERE DocumentID = ?", (key_document_id,))
+    key_media_count = cursor.fetchone()[0]
 
-# 유사한 미디어를 찾는 함수
-def find_similar_media(hashes, key_hashes, key_document_id, threshold=5):
-    similar_media = []
-    for file_id, (document_id, image_hash) in hashes.items():
-        if document_id != key_document_id:
-            for key_hash in key_hashes:
-                difference = key_hash - image_hash
-                if difference <= threshold:
-                    similar_media.append((key_document_id, document_id, file_id, difference))
-    return similar_media
+    # 모든 문서의 미디어 파일 수를 계산합니다.
+    cursor.execute("SELECT DocumentID, COUNT(*) as TotalCount FROM MediaFiles GROUP BY DocumentID")
+    total_media_count = {row['DocumentID']: row['TotalCount'] for row in cursor.fetchall()}
 
+    # key_document_id를 제외한 모든 미디어 파일의 해시값과 원본 파일 이름을 가져옵니다.
+    cursor.execute("SELECT DocumentID, SHA256Hash, SourceFileName FROM MediaFiles WHERE DocumentID != ?", (key_document_id,))
+    all_other_files = cursor.fetchall()
+
+    for row in all_other_files:
+        doc_id = row['DocumentID']
+        if row['SHA256Hash'] in key_files_hashes:
+            if doc_id not in similar_documents:
+                similar_documents[doc_id] = {'matched_count': 0, 'files': set()}
+            similar_documents[doc_id]['matched_count'] += 1
+            similar_documents[doc_id]['files'].add(row['SourceFileName'])
+
+    # 결과를 정리하여 리턴합니다.
+    result = []
+    for doc_id, info in similar_documents.items():
+        file_names_str = ", ".join(info['files'])  # 파일 이름 목록을 문자열로 변환
+        match_ratio = f"{info['matched_count']}/{key_media_count}"  # 일치 비율 계산
+        target_media_count = total_media_count.get(doc_id, 0)  # 대상 파일 미디어 전체 개수
+        result.append({
+            'DocumentID': doc_id,
+            'TargetMediaCount': target_media_count,
+            'SourceFileNames': file_names_str,
+            'MatchRatio': match_ratio
+            
+        })
+
+    return result
 # 완전히 동일한 해시값을 가져와서 문서를 찾는 함수
 def find_identical_documents(conn, key_document_id):
     identical_documents = []
