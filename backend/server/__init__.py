c2ce55044897567f32e5917644e9c83d1ec1c85a
dianin5 <xyg1940@cu.ac.kr>
2023-12-07 14:29:43 +0900

    이메일 따로 분리한거 합침

--- backend/server/__init__.py
+++ backend/server/__init__.py
@@ -13,8 +13,6 @@ from .similarity import similarity_bp
 from .search import search_bp
 from .email import email_bp
 from .emlperson import emailperson_bp
-from .tag import tag_bp
-from .nnp import nnp_bp
 
 def create_app():
     app = Flask(__name__)
@@ -32,8 +30,6 @@ def create_app():
     app.register_blueprint(search_bp)
     app.register_blueprint(email_bp)
     app.register_blueprint(emailperson_bp)
-    app.register_blueprint(tag_bp)
-    app.register_blueprint(nnp_bp)
 #    app.register_blueprint(calendar_bp)
 
 
