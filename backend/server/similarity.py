5b0373b21bc095cea8e8f7cb0ce47a0deae969ca
dianin5 <xyg1940@cu.ac.kr>
2023-12-07 12:45:29 +0900

    코드 최적화

--- backend/server/similarity.py
+++ backend/server/similarity.py
@@ -15,8 +15,7 @@ def similarity_analysis():
     key_document_id = data['key_document_id']
 
     with sqlite3.connect(db_path) as conn:
-        # 메타데이터 처리 및 해시 계산
-        process_files(conn)
+        conn.row_factory = sqlite3.Row
         # 유사한 미디어 파일이 포함된 문서 찾기
         similar_media = find_similar_media(conn, key_document_id)
         
@@ -33,67 +32,6 @@ def similarity_analysis():
     }
 
     return jsonify(result)
-
-# OOXML 파일이 맞는지 확인하는 함수
-def is_ooxml(filename):
-    ooxml_extensions = ['.docx', '.xlsx', '.pptx']
-    _, extension = os.path.splitext(filename)
-    return extension in ooxml_extensions
-
-# OOXML 파일에서 메타데이터를 추출하는 함수
-def parse_metadata(blob_data, xml_file):
-    with ZipFile(BytesIO(blob_data), 'r') as zip:
-        with zip.open(xml_file) as xml:
-            tree = ET.parse(xml)
-            root = tree.getroot()
-            metadata = {child.tag.split('}')[-1]: child.text for child in root}
-            return metadata
-
-# 데이터베이스에 메타데이터를 저장하는 함수
-def save_metadata(conn, filename, metadata):
-    cursor = conn.cursor()
-    # 이미 생성된 documentmetadata 테이블의 현재 컬럼을 확인
-    cursor.execute("PRAGMA table_info(documentmetadata);")
-    existing_columns = {info[1] for info in cursor.fetchall()}
-
-    # 새로운 메타데이터 키에 대해 필요한 경우 컬럼 추가
-    for key in metadata.keys():
-        if key not in existing_columns:
-            cursor.execute(f'ALTER TABLE documentmetadata ADD COLUMN "{key}" TEXT;')
-            conn.commit()
-
-    # 기존에 해당 파일명으로 된 레코드가 있는지 확인
-    cursor.execute("SELECT * FROM documentmetadata WHERE filename = ?", (filename,))
-    row = cursor.fetchone()
-
-    # 레코드가 없으면 새로운 레코드를 삽입
-    if row is None:
-        columns = '", "'.join(metadata.keys())
-        placeholders = ', '.join('?' * len(metadata))
-        sql = f'INSERT INTO documentmetadata ("filename", "{columns}") VALUES (?, {placeholders})'
-        values = [filename] + list(metadata.values())
-        cursor.execute(sql, values)
-        conn.commit()
-    else:
-        # 레코드가 있으면 새로운 메타데이터 값으로 업데이트
-        for key, value in metadata.items():
-            if row[key] is None:
-                sql = f'UPDATE documentmetadata SET "{key}" = ? WHERE filename = ?'
-                cursor.execute(sql, (value, filename))
-                conn.commit()
-
-# OOXML 파일의 메타데이터를 처리하는 함수
-def process_files(conn):
-    conn.row_factory = sqlite3.Row  
-    cursor = conn.cursor()
-    cursor.execute("SELECT file_path, blob_data FROM files")
-    rows = cursor.fetchall()
-    for row in rows:
-        file_path, blob_data = row['file_path'], row['blob_data']
-        if is_ooxml(file_path):
-            for xml_file in ['docProps/app.xml', 'docProps/core.xml']:
-                metadata = parse_metadata(blob_data, xml_file)
-                save_metadata(conn, file_path, metadata)
 
 def find_similar_media(conn, key_document_id):
     similar_documents = {}
